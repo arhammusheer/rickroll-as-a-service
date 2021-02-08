e82c14e7c1711883c35cdc34b789fb8bacc94c12
arhammusheer <arhammusheer@gmail.com>
2021-02-08 18:25:40 +0530

    removed sass

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const sassMiddleware = require("node-sass-middleware");
 const mongoose = require("mongoose");
 
 const indexRouter = require("./routes/index");
@@ -34,14 +33,6 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(
-  sassMiddleware({
-    src: path.join(__dirname, "public"),
-    dest: path.join(__dirname, "public"),
-    indentedSyntax: true, // true = .sass and false = .scss
-    sourceMap: true,
-  })
-);
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
